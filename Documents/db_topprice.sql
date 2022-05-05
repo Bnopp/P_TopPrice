5c452abcf3b3fc8201f2a76f6a9ff18448708586
pl03jlu <pl03jlu@eduvaud.ch>
2022-05-05 21:41:00 +0200

    Updated Database

--- Documents/db_topprice.sql
+++ Documents/db_topprice.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 23, 2022 at 07:34 PM
+-- Generation Time: May 05, 2022 at 07:36 PM
 -- Server version: 5.7.11
 -- PHP Version: 8.0.1
 
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 -- Database: `db_topprice`
 --
 
-CREATE DATABASE db_topprice;
+Create Database 'db_topprice';
 
 -- --------------------------------------------------------
 
@@ -35,6 +35,13 @@ CREATE TABLE `t_brand` (
   `fkOS` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `t_brand`
+--
+
+INSERT INTO `t_brand` (`idBrand`, `braName`, `fkOS`) VALUES
+(1, 'Apple Inc.', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +53,13 @@ CREATE TABLE `t_constructor` (
   `conName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `t_constructor`
+--
+
+INSERT INTO `t_constructor` (`idConstructor`, `conName`) VALUES
+(1, 'Foxconn');
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +70,13 @@ CREATE TABLE `t_os` (
   `idOS` int(11) NOT NULL,
   `osName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_os`
+--
+
+INSERT INTO `t_os` (`idOS`, `osName`) VALUES
+(1, 'IOS');
 
 -- --------------------------------------------------------
 
@@ -81,6 +102,13 @@ CREATE TABLE `t_provider` (
   `proName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `t_provider`
+--
+
+INSERT INTO `t_provider` (`idProvider`, `proName`) VALUES
+(1, 'Digitec Galaxus AG');
+
 -- --------------------------------------------------------
 
 --
@@ -90,18 +118,29 @@ CREATE TABLE `t_provider` (
 CREATE TABLE `t_smartphone` (
   `idSmartphone` int(11) NOT NULL,
   `smaModel` varchar(255) NOT NULL,
-  `smaPrice` double NOT NULL,
+  `smaPrice` decimal(10,0) NOT NULL,
   `smaStorage` int(11) NOT NULL,
-  `smaBattery` int(11) NOT NULL,
+  `smaBatteryLife` int(11) NOT NULL,
   `smaRAM` int(11) NOT NULL,
+  `smaScreenSize` decimal(10,0) DEFAULT NULL,
   `smaResolution` varchar(255) NOT NULL,
   `smaCPU` varchar(255) DEFAULT NULL,
-  `smaClockSpeed` double DEFAULT NULL,
-  `smaThreads` double DEFAULT NULL,
+  `smaClockSpeed` decimal(10,0) DEFAULT NULL,
+  `smaThreads` decimal(10,0) DEFAULT NULL,
   `fkConstructor` int(11) DEFAULT NULL,
   `fkOS` int(11) DEFAULT NULL,
   `fkProvider` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_smartphone`
+--
+
+INSERT INTO `t_smartphone` (`idSmartphone`, `smaModel`, `smaPrice`, `smaStorage`, `smaBatteryLife`, `smaRAM`, `smaScreenSize`, `smaResolution`, `smaCPU`, `smaClockSpeed`, `smaThreads`, `fkConstructor`, `fkOS`, `fkProvider`) VALUES
+(1, 'iPhone 13', '879', 128, 19, 4, '6', '1170 x 2532', 'A15 Bionic', '3', '6', 1, 1, 1),
+(2, 'iPhone 12', '839', 128, 17, 4, '6', '1170 x 2532', 'A14 Bionic', '3', '6', 1, 1, 1),
+(3, 'iPhone 11', '609', 128, 17, 4, '6', '828 x 1792', 'A13 Bionic', '3', '6', 1, 1, 1),
+(4, 'iPhone SE', '538', 128, 9, 4, '5', '750 x 1334', 'A15 Bionic', '3', '6', 1, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -149,31 +188,31 @@ ALTER TABLE `t_smartphone`
 -- AUTO_INCREMENT for table `t_brand`
 --
 ALTER TABLE `t_brand`
-  MODIFY `idBrand` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idBrand` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `t_constructor`
 --
 ALTER TABLE `t_constructor`
-  MODIFY `idConstructor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idConstructor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `t_os`
 --
 ALTER TABLE `t_os`
-  MODIFY `idOS` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idOS` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `t_provider`
 --
 ALTER TABLE `t_provider`
-  MODIFY `idProvider` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProvider` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `t_smartphone`
 --
 ALTER TABLE `t_smartphone`
-  MODIFY `idSmartphone` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idSmartphone` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
