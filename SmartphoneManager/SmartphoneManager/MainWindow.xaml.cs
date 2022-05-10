b948fe90eb026585449daff392d2863c41eabad5
pl03jlu <pl03jlu@eduvaud.ch>
2022-05-10 16:36:40 +0200

    Home Page Almost finished
    no dpi-aware option

--- SmartphoneManager/SmartphoneManager/MainWindow.xaml.cs
+++ SmartphoneManager/SmartphoneManager/MainWindow.xaml.cs
@@ -13,6 +13,10 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using SmartphoneManager.Pages;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml.Interop;
+using Microsoft.UI;
+using System.Runtime.InteropServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,11 +28,25 @@ namespace SmartphoneManager
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private AppWindow _apw;
+        private OverlappedPresenter _presenter;
+
         public MainWindow()
         {
             this.InitializeComponent();
 
+            GetAppWindowAndPresenter();
+            _presenter.IsResizable = false;
+
             ContentFrame.Navigate(typeof(WelcomePage));
+        }
+
+        public void GetAppWindowAndPresenter()
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            _apw = AppWindow.GetFromWindowId(myWndId);
+            _presenter = _apw.Presenter as OverlappedPresenter;
         }
     }
 }
